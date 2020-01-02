b72fd4388f5f6c065b39ef51d67a8551d3cf4106
Aiden <nikangdi@aliyun.com>
2020-01-02 20:08:59 +0800

    electron-pc-nodejs

---
+++ index.js
@@ -0,0 +1,12 @@
+const {app,BrowserWindow} = require('electron')
+
+app.on('ready',()=>{
+    const win = new BrowserWindow({
+        width:540,
+        height:720,
+        frame:false,
+        resizable:false
+    })
+    // win.webContents.openDevTools()
+    win.loadFile('./layout/index.html')
+})
